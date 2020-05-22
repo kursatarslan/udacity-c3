9f64ed8ea238fd508bb26311c833e604d210af11
TTKARSLAN <kursat.arslan@turkcell.com.tr>
2020-05-22 23:26:20 +0300

    remove node_modules

---
+++ udacity-c3-deployment/docker/docker_push.sh
@@ -0,0 +1,4 @@
+docker push kursatarslan/reverseproxy &&
+docker push kursatarslan/udacity-restapi-user &&
+docker push kursatarslan/udacity-restapi-feed &&
+docker push kursatarslan/udacity-frontend:local 
---
+++ udacity-c3-deployment/k8s/delete_all.sh
@@ -0,0 +1,10 @@
+kubectl delete -f backend-feed-service.yaml &&
+kubectl delete -f backend-user-service.yaml &&
+kubectl delete -f frontend-service.yaml &&
+kubectl delete -f env-configmap.yaml &&
+kubectl delete -f env-secret.yaml &&
+kubectl delete -f backend-feed-deployment.yaml &&
+kubectl delete -f backend-user-deployment.yaml &&
+kubectl delete -f frontend-deployment.yaml &&
+kubectl delete -f reverseproxy-service.yaml &&
+kubectl delete -f reverseproxy-deployment.yaml
